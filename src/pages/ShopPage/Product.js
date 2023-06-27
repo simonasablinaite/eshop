9a0cb6cfb738b5cf4a3641f0ced4a8ed6171d34d
Simona <simonasablinaite@gmail.com>
2023-06-27 09:30:21 +0300

    Updates

--- src/pages/ShopPage/Product.js
+++ src/pages/ShopPage/Product.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { API_URL } from '../../config';
 import basketCart from '../../img/basket-cart-icon.png'
 
